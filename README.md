257ba7aaed95fa4cbcd73b80f1da6d11be8d14a4
rabbitmq <pr279@njit.edu>
2020-03-09 19:12:48 -0400

    configured rabbitmq .ini and .php files

---
+++ README.md
@@ -0,0 +1,2 @@
+# it490-call of duty
+This is a college senior year project. A team of 3 people's, working on system integration project.
---
+++ mysqlconnect.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+
+$mydb = new mysqli('10.0.0.22','ubuntu','ubuntu','IT490');
+
+if ($mydb->errno != 0)
+{
+	echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+	exit(0);
+}
+
+echo "successfully connected to database".PHP_EOL;
+
+$query = "select * from Info;";
+
+$response = $mydb->query($query);
+if ($mydb->errno != 0)
+{
+	echo "failed to execute query:".PHP_EOL;
+	echo __FILE__.':'.__LINE__.":error: ".$mydb->error.PHP_EOL;
+	exit(0);
+}
+
+
+?>
