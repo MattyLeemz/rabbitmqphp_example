09baa1300a77923038227016f521ecb5a16c94c8
MattyLeemz <ml396@njit.edu>
2020-03-09 21:23:40 -0400

    changed the test client for our credentials

--- html/Midterm/frontend/rabbitmqphp_example/testRabbitMQClient.php
+++ html/Midterm/frontend/rabbitmqphp_example/testRabbitMQClient.php
@@ -5,19 +5,12 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "test message";
-}
+
 
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['username'] = $_POST["username"];
+$request['password'] = $_POST["password"];
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
